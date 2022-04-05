de1affd733bb908bad1e52a6903fdf664a9d5b4b
Jefferson Hita <53319352+hitasan@users.noreply.github.com>
2022-04-04 23:23:02 -0300

    Seller TableView

--- secao23/src/gui/util/Utils.java
+++ secao23/src/gui/util/Utils.java
@@ -1,7 +1,13 @@
 package gui.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class Utils {
@@ -9,7 +15,7 @@ public class Utils {
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
-	
+
 	// Metodo para auxilio em conversao de valor da caixa de texto para inteiro
 	public static Integer tryParseToInt(String str) {
 		try {
@@ -19,5 +25,44 @@ public class Utils {
 		} catch (NumberFormatException e) {
 			return null;
 		}
+	}
+
+	// Metodo para formatar a data
+	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
+				private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+				@Override
+				protected void updateItem(Date item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
+				}
+			};
+			return cell;
+		});
+	}
+
+	// Metodo para formatar valores com ponto flutuante
+	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+			return cell;
+		});
 	}
 }
